53d3bc8fdeb03a0961a512448606f3648f643661
aclassiter <a.class.wa@gmail.com>
2022-04-20 09:19:54 -0700

    Assessment complete. Pseudocode explains issues with completing final code challenge.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,35 +15,105 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
-
+//Test: 
+describe("shuffleArray", () => {
+    //describe what the test does
+    it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+        //variables given
+        const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+        // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+        const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+        // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+        //expected outputs
+        expect(shuffleArray(colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
+        expect(shuffleArray(colors2)).toEqual(expect.arrayContaining(["aquamarine", "indigo", "saffron", "periwinkle", "ochre"]))
+    })
+})
+//Fail received: 
+//ReferenceError: shuffleArray is not defined
 
 // b) Create the function that makes the test pass.
+//Function name: shuffleArray
+//Parameter: array
+//Function takes in an array, removes the first item from the array and shuffles the remaining content
+//Use slice() method at first index
+//Use array.sort() to sort through negative and positive numbers 
+//Use Math.random() - 0.5 to create a random positive or negative number
 
+const shuffleArray = (array) => {
+    return array.slice(1).sort(() => Math.random() - 0.5)
+}
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+// const nums1 = [3, 56, 90, -8, 0, 23, 6]
+// // Expected output: [-8, 90]
+// const nums2 = [109, 5, 9, 67, 8, 24]
+// // Expected output: [5, 109]
 
+// //Test: 
+describe("minMaxNumbers", () => {
+    //describe what the test does
+    it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
+      //variables given
+      const nums1 = [3, 56, 90, -8, 0, 23, 6]
+      // Expected output: [-8, 90]
+      const nums2 = [109, 5, 9, 67, 8, 24]
+      // Expected output: [5, 109]
+    //expected outputs
+    expect(minMaxNumbers(nums1)).toEqual([-8, 90])
+    expect(minMaxNumbers(nums2)).toEqual([5, 109])
+    })
+
+})
+//Fail received: 
+//ReferenceError: minMaxNumbers is not defined
 
 // b) Create the function that makes the test pass.
+//Function name: minMaxNumbers
+//Parameter: array of numbers
+//Function takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+//Use Math.min() to return the number in the array with the lowest value.
+//Use Math.max() to return the number in the array with the highest value.
+const minMaxNumbers = (array) => {
+    return [Math.min(...array), Math.max(...array)]
+}
 
-
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+//Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+// --------------------3) 
 
 // a) Create a test with an expect statement using the variables provided.
 
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+// const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+// const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+// // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+//Test: 
+describe("noDuplicateArray", () => {
+    //describe what the test does
+    it("takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+      //variables given
+      const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+      const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+      const testArray3= testArray1.concat(testArray2).unique()
+    //expected outputs
+    //Here, I'm having trouble determining how to show our output as a single array by combining testArray1 and testArray2. I keep receiving a failed test result of "Your test suite must contain at least one test."
+    expect(noDuplicateArray(testArray3)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
 
+})
 
 // b) Create the function that makes the test pass.
+//Function name: noDuplicateArray
+//Parameter: arrays 
+//Function takes in two arrays as arguments and returns one array with no duplicate values.
+//Use .concat method to combine arrays
+//Use .unique to remove duplicate values
+//I believe I need to create a third variable called testArray3 that contains both arrays concatenated, but this isn't working in the test. 
+const testArray3 = (array) => {
+    return testArray3
+}
+//I am still getting the same error. I believe the function works properly, although I would have it written with the variable in the return line, showing concatenation with testArray1 and testArray2. 
+
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -9,8 +9,20 @@ num2 = 42
 # Expected output: '42 is even'
 num3 = 221
 # Expected output: '221 is odd'
+# #Method name: if_even_or_odd
+# #Parameter: number
+# #Use conditional statement with .even and .odd methods to determine if number is even or odd. 
+def if_even_or_odd(number)
+    if number.even?
+        "#{number} is even"
+    else 
+        "#{number} is odd"
+    end
+end 
 
-
+p if_even_or_odd(num1)
+p if_even_or_odd(num2)
+p if_even_or_odd(num3)
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
@@ -21,6 +33,16 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# #Method name: remove_vowels
+# #Parameter: string
+# #Use .delete method to remove vowels, identifying lowercase and uppercase vowels 
+def remove_vowels(string)
+    string.delete('aeiouAEIOU')
+end
+
+p remove_vowels(album1)
+p remove_vowels(album2)
+p remove_vowels(album3)
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
@@ -31,3 +53,21 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+#Method name: palindrome_string
+#Parameter: string
+#Use conditional statement to determine if the input string is a palindrome (does the string read the same in reverse?)
+#Use .downcase method to return a string with its uppercase letters replaced by lowercase letters, for palindrome evaluation
+#Use .reverse method to return the input string in reverse 
+
+def palindrome_string(string)
+    if string.downcase == string.downcase.reverse
+        "#{string} is a palindrome"
+    else 
+        "#{string} is not a palindrome"
+    end 
+end
+
+    p palindrome_string(palindrome_tester1)
+    p palindrome_string(palindrome_tester2)
+    p palindrome_string(palindrome_tester3)
